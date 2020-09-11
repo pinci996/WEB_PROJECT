42f038a08f2061297355b99c8db90a79f7b83969
pinci996 <44240764+pinci996@users.noreply.github.com>
2020-09-11 04:20:34 +0200

    Update register.php

--- register.php
+++ register.php
@@ -69,50 +69,50 @@ crossorigin="anonymous"></script>
 </html>
 
 <?php
-// Change this to your connection info.
+
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'project';
-// Try and connect using the info above.
+
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
+	
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 if (!isset($_POST['email'], $_POST['username'], $_POST['lozinka'])) {
-	// Could not get the data that should have been sent.
+	
 	die ('Please complete the registration form!');
 }
-// Make sure the submitted registration values are not empty.
+
 if (empty($_POST['email']) || empty($_POST['username']) || empty($_POST['lozinka'])) {
-	// One or more values are empty.
+	
 	die ('Please complete the registration form');
 }
 if ($stmt = $con->prepare('SELECT id, lozinka FROM korisnici WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+	
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	$stmt->store_result();
-	// Store the result so we can check if the account exists in the database.
+	
 	if ($stmt->num_rows > 0) {
-		// Username already exists
+		
 		echo 'Username exists, please choose another!';
 	} else {
 		if ($stmt = $con->prepare('INSERT INTO korisnici (email, username, lozinka) VALUES (?, ?, ?)')) {
-                        // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+                        
                         
                         $stmt->bind_param('sss', $_POST['email'],$_POST['username'],$_POST['lozinka']);
                         $stmt->execute();
                         echo 'You have successfully registered, you can now login!';
                 } else {
-                        // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+                        
                         echo 'Could not prepare statement!';
                 }
 	}
 	$stmt->close();
 } else {
-	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+	
 	echo 'Could not prepare statement!';
 }
 $con->close();
